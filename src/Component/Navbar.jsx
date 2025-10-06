1bcd9579569ff7b131748c55a8faa5f422065f7e
Anurag Sharma <anuragwork2005@gmail.com>
2025-10-06 22:40:32 +0530

    Adding navbar heading

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
     >
       {/* Left side - Project name */}
       <h1
-        className="text-2xl sm:text-2xl md:text-4xl font-extrabold text-center bg-gradient-to-r from-pink-400 via-purple-400 to-blue-400 bg-clip-text text-transparent font-[Playfair_Display] cursor-pointer whitespace-nowrap md:whitespace-normal"
+        className="text-2xl sm:text-1xl md:text-4xl font-extrabold text-center bg-gradient-to-r from-pink-400 via-purple-400 to-blue-400 bg-clip-text text-transparent font-[Playfair_Display] cursor-pointer whitespace-nowrap md:whitespace-normal"
         onClick={() => navigate("/")} // ✅ use navigate()
       >
         Professional Ethics
